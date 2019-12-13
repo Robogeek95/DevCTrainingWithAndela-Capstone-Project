dc812956728d8ead9203c957afa7a9c179d6378f
Robogiek <azeezlukman95@gmail.com>
2019-12-13 13:32:12 +0100

    remove unnecessary files

--- models/article-model.js
+++
@@ -1,7 +0,0 @@
-const pool = require('../config/queries');
-
-class articleModel {
-  
-}
-
-module.exports = articleModel;
--- models/feeds-model.js
+++
@@ -1,7 +0,0 @@
-const pool = require('../config/queries');
-
-class feedsModel {
-    
-}
-
-module.exports = feedsModel;
