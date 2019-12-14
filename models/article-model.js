af06a681b3121b1cf9da37f38a0bdb9aa345790d
Robogiek <azeezlukman95@gmail.com>
2019-12-14 11:13:30 +0100

    model to enable test

--- models/article-model.js
+++ models/article-model.js
@@ -1,7 +1,26 @@
 const pool = require('../config/queries');
 
 class articleModel {
-  
+    static async getComments(userId) {
+        const query = 'select commentid, comment, email from article_comments  inner join users on  article_comments.userid = users.userid where articleid = $1';
+        const values = [`${userId}`];
+        const result = pool.query(query, values);
+        return result;
+    }
+
+    static async getTest(article) {
+        const query = 'select * from articles where title = $1';
+        const value = [`${article.title}`];
+        const result = pool.query(query, value);
+        return result;
+    }
+
+    static async deleteTests(article) {
+        const query = 'delete from articles where title = $1'
+        const value = [`${article.title}`];
+        const result = pool.query(query, value);
+        return result;
+    }
 }
 
 module.exports = articleModel;
--- models/gif-model.js
+++ models/gif-model.js
@@ -1,7 +1,26 @@
 const pool = require('../config/queries');
 
 class gifModel {
- 
+    static async getComments(userId) {
+        const query = 'select commentid, comment, email from gif_comments inner join users on  gif_comments.userid = users.userid where gifid = $1';
+        const values = [`${userId}`];
+        const result = pool.query(query, values);
+        return result;
+    }
+
+    static async getTest(gif) {
+        const query = 'select gifid from gifs where title = $1';
+        const values = [`${gif.title}`];
+        const result = pool.query(query, values);
+        return result;
+    }
+
+    static async deleteTests(gif) {
+        const query = 'delete from gifs where title = $1'
+        const value = [`${gif.title}`];
+        const result = pool.query(query, value);
+        return result;
+    }
 }
 
 module.exports = gifModel;
