351b90e9414c9e1a9891c23872800a40f5f8c553
Robogiek <azeezlukman95@gmail.com>
2019-12-14 09:51:58 +0100

    removed unused files

--- controllers/article-controller.js
+++ controllers/article-controller.js
@@ -2,3 +2,42 @@
 const model = require('../models/article-model');
 
 const getUserId = require('../middleware/user-id');
+
+exports.editArticle = async (req, res, next) => {
+    const userId = await getUserId(req);
+    const { id } = req.params;
+    const article = {
+      id,
+      title: req.body.title,
+      article: req.body.article,
+      userId,
+    };
+  
+    model.update(article).then(
+      (data) => {
+        if (data.rowCount === 0) {
+          res.status(500).json({
+            message: 'article does not exist',
+          });
+        }
+  
+        res.status(201).json({
+          status: 'success',
+          data: {
+            message: 'Article successfully updated',
+            articleId: data.rows[0].id,
+            createdOn: data.rows[0].createDate,
+            title: data.rows[0].title,
+            article: data.rows[0].article,
+          },
+        });
+      },
+    ).catch(
+      (error) => {
+        res.status(400).json({
+          message: `${error} occoured while saving to Database`,
+        });
+        throw error;
+      },
+    );
+  };
--- models/article-model.js
+++ models/article-model.js
@@ -1,7 +1,12 @@
 const pool = require('../config/queries');
 
 class articleModel {
-  
+    static async update(article) {
+        const query = 'UPDATE articles SET title = $1, article = $2 WHERE articleid = $3 RETURNING *';
+        const values = [`${article.title}`, `${article.article}`, `${article.id}`];
+        const result = pool.query(query, values);
+        return result;
+      }
 }
 
 module.exports = articleModel;
