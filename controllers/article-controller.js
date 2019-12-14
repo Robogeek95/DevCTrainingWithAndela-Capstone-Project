f7e5cff922626a66efecf8fc8310e5900729a500
Robogiek <azeezlukman95@gmail.com>
2019-12-14 10:23:22 +0100

    Employees can comment on other colleagues' article post.

--- controllers/article-controller.js
+++ controllers/article-controller.js
@@ -2,3 +2,42 @@
 const model = require('../models/article-model');
 
 const getUserId = require('../middleware/user-id');
+
+exports.commentArticle = async (req, res, next) => {
+    const userId = await getUserId(req);
+    const articleId = req.params.id;
+    const comment = {
+        comment: req.body.comment,
+        articleId,
+        userId,
+    };
+
+    await model.comment(comment).then(
+        (data) => {
+            if (data.rowCount === 0) {
+                res.status(500).json({
+                    message: 'article does not exist',
+                });
+            }
+
+            res.status(200).json({
+                status: 'success',
+                data: {
+                    message: 'Comment successfully created',
+                    createdOn: data.rows[0].createdon,
+                    articleTitle: data.rows[0].title,
+                    article: data.rows[0].article,
+                    comment: data.rows[0].comment,
+                },
+            });
+        },
+    ).catch(
+        (error) => {
+            res.status(500).json({
+                message: `error ${error} occoured while adding comment`,
+            });
+            console.log(error);
+            throw error;
+        },
+    );
+};
--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,9 @@ const router = express.Router();
 
 const auth = require('../middleware/auth');
 
-// const gifctrl = require('../controllers/gif-controller');
-// const articlectrl = require('../controllers/article-controller');
-// const feedsctrl = require('../controllers/feeds-controller');
+const articlectrl = require('../controllers/article-controller');
+
+router.post('/articles/:id/comment', auth, articlectrl.commentArticle);
 
 
 module.exports = router;
