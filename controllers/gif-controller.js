950a6bed59a11872413ae6cc3e08018bd540af24
Robogiek <azeezlukman95@gmail.com>
2019-12-14 10:38:41 +0100

    Employee can comment on other colleague's gif post

--- controllers/gif-controller.js
+++ controllers/gif-controller.js
@@ -1,6 +1,42 @@
 
 const model = require('../models/gif-model');
 
-const cloudstorage = require('../middleware/cloud-config');
-
 const getUserId = require('../middleware/user-id');
+
+
+exports.commentGif = async (req, res, next) => {
+    const userId = await getUserId(req);
+    const gifId = req.params.id;
+    const comment = {
+        comment: req.body.comment,
+        gifId,
+        userId,
+    };
+
+    await model.comment(comment).then(
+        (data) => {
+            if (data.rowCount === 0) {
+                res.status(500).json({
+                    message: 'gif does not exist',
+                });
+            }
+
+            res.status(200).json({
+                status: 'success',
+                data: {
+                    message: 'Comment successfully created',
+                    createdOn: data.rows[0].createdon,
+                    gifTitle: data.rows[0].title,
+                    comment: data.rows[0].comment,
+                },
+            });
+        },
+    ).catch(
+        (error) => {
+            res.status(500).json({
+                message: `error ${error} occoured while adding comment`,
+            });
+            throw error;
+        },
+    );
+};
--- models/gif-model.js
+++ models/gif-model.js
@@ -1,7 +1,30 @@
 const pool = require('../config/queries');
 
 class gifModel {
- 
+    static async comment(comment) {
+        function writeComment() {
+            const query = 'INSERT INTO gif_comments (comment, gifid, userid) VALUES($1, $2, $3) RETURNING *';
+            const values = [`${comment.comment}`, `${comment.gifId}`, `${comment.userId}`];
+            const result = pool.query(query, values);
+            return result;
+        }
+
+        let commentid;
+        await writeComment().then(
+            (data) => {
+                commentid = data.rows[0].commentid;
+            },
+        ).catch(
+            (error) => {
+                throw error;
+            },
+        );
+
+        const query = 'SELECT gif_comments.createdon, gifs.title, gif_comments.comment FROM gifs INNER JOIN gif_comments ON gifs.gifid = gif_comments.gifid WHERE commentid = $1';
+        const values = [commentid];
+        const result = pool.query(query, values);
+        return result;
+    }
 }
 
 module.exports = gifModel;
