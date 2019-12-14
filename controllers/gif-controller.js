957fc945fe4be12cfb32484b1a293e1fc3d39ebd
Robogiek <azeezlukman95@gmail.com>
2019-12-14 11:10:15 +0100

    Employees can view a specific gif post

--- controllers/gif-controller.js
+++ controllers/gif-controller.js
@@ -1,6 +1,59 @@
 
 const model = require('../models/gif-model');
 
-const cloudstorage = require('../middleware/cloud-config');
-
 const getUserId = require('../middleware/user-id');
+
+exports.getGif = async (req, res, next) => {
+    const { id } = req.params;
+    const gif = { id };
+    await model.getOneGif(gif).then(
+        (data) => {
+            if (data.rowCount === 0) {
+                res.status(500).json({
+                    message: 'gif does not exist',
+                });
+            }
+
+            const comments = [];
+            model.getComments(id).then(
+                (commentdata) => {
+                    for (let i = 0; i < commentdata.rowCount; i += 1) {
+                        const tempcomments = {
+                            commentId: commentdata.rows[i].commentid,
+                            comment: commentdata.rows[i].comment,
+                            authorId: commentdata.rows[i].email,
+                        };
+                        comments.push(tempcomments);
+                    }
+
+                    res.status(201).json({
+                        data: {
+                            status: 'success',
+                            data: {
+                                data: data.rows[0].gifid,
+                                createdOn: data.rows[0].createdate,
+                                title: data.rows[0].title,
+                                url: data.rows[0].imageurl,
+                                comments,
+                            },
+                        },
+                    });
+                },
+            ).catch(
+                (error) => {
+                    res.status(500).json({
+                        message: 'error occoured while fetching comments',
+                    });
+                    throw error;
+                },
+            );
+        },
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                message: `${error} occoured while fetching article from database`,
+            });
+            throw error;
+        },
+    );
+};
--- models/gif-model.js
+++ models/gif-model.js
@@ -1,7 +1,12 @@
 const pool = require('../config/queries');
 
 class gifModel {
- 
+    static async getOneGif(gif) {
+        const query = 'select gifid, createdate, title, imageurl from gifs inner join users on gifs.userid = users.userid where gifid = $1';
+        const values = [`${gif.id}`];
+        const result = pool.query(query, values);
+        return result;
+    }
 }
 
 module.exports = gifModel;
