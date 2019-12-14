5c97db1da354e6b88fc3c8ff35750987804a94b6
Robogiek <azeezlukman95@gmail.com>
2019-12-14 10:17:41 +0100

    delete line

--- controllers/gif-controller.js
+++ controllers/gif-controller.js
@@ -1,8 +1,6 @@
 
 const model = require('../models/gif-model');
 
-const cloudstorage = require('../middleware/cloud-config');
-
 const getUserId = require('../middleware/user-id');
 
 exports.deleteGif = async (req, res, next) => {
